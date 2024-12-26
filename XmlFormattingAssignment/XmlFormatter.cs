48884192e4b4a2274e0610c2d091f5eac14fcd15
ZahirRussell <zirussell007@live.com>
2024-12-26 17:17:59 +0600

    SerializeObject Method Done and Test

--- XmlFormattingAssignment/XmlFormatter.cs
+++ XmlFormattingAssignment/XmlFormatter.cs
@@ -16,6 +16,7 @@ namespace XmlFormattingAssignment
             StringBuilder sb = new StringBuilder();
             Type type = obj.GetType();
             //ToDo XML Serialize
+            SerializeObject(obj, type.Name, sb);
             return sb.ToString();
         }
 
@@ -27,9 +28,57 @@ namespace XmlFormattingAssignment
                 AppendTag(xmlBuilder, tagName, null);
                 return;
             }
+
+            Type type = obj.GetType();
+            //ToDO Append Plain Object
+            if (type.IsPrimitive ||
+                   type.IsEnum ||
+                   type == typeof(string) ||
+                   type == typeof(decimal) ||
+                   type == typeof(DateTime) ||
+                   type == typeof(Guid))
+            {
+                AppendTag(xmlBuilder,tagName,obj.ToString());
+            }
+            else if (typeof(IEnumerable).IsAssignableFrom(type))
+            {
+                AppendOpeningTag(xmlBuilder, tagName); // set openining tag
+
+                foreach (var item in (IEnumerable)obj)
+                {
+                    SerializeObject(item, item.GetType().Name, xmlBuilder);
+                }
+
+                AppendClosingTag(xmlBuilder, tagName); // set closing tag
+            }
+            else
+            {
+                AppendOpeningTag(xmlBuilder, tagName); // set opening tag
+
+                foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (property.CanRead)
+                    {
+                        object value = property.GetValue(obj);
+                        SerializeObject(value, property.Name, xmlBuilder);
+                    }
+                }
+
+                AppendClosingTag(xmlBuilder, tagName); // set closing tag
+            }
         }
 
         private static void AppendTag(StringBuilder xmlBuilder, string tagName, string value) { xmlBuilder.Append(tagName); xmlBuilder.Append(value); }
+
+        private static void AppendOpeningTag(StringBuilder xmlBuilder, string tagName)
+        {
+            xmlBuilder.AppendLine($"<{tagName}>");
+        }
+
+        private static void AppendClosingTag(StringBuilder xmlBuilder, string tagName)
+        {
+            xmlBuilder.AppendLine($"</{tagName}>");
+        }
 
     }
 }
