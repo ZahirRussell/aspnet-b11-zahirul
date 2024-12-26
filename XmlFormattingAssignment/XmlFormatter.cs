209a0b25ea14355c9931417d30c53bf4bf439f7d
ZahirRussell <zirussell007@live.com>
2024-12-26 16:10:18 +0600

    Add project files.

---
+++ XmlFormattingAssignment/XmlFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Reflection;
+using System.Text;
+
+namespace XmlFormattingAssignment
+{
+    public static class XmlFormatter
+    {
+        public static string Convert(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
